10365f81c4a52d24c68bdd72a700a8cef200b8d8
TwoTu <zhu_xiangkun@qq.com>
2022-05-21 10:52:32 +0800

    Update R3G firmware

---
+++ configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Modify default system settings
+
+# 修改默认IP为192.168.123.1
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate 
+
+# Hello World
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# luci-theme-infinityfreedom
+echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
+
+# passwall
+echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+
+# 替换默认主题
+rm -rf package/lean/luci-theme-argon 
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone  https://github.com/bigbugcc/OpenwrtApp package/OpenwrtApp
+git clone  https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone  https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+# vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
